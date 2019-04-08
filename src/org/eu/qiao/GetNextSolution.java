4212f78dc91b7b06acd2ad3ccdd05fdcb484a4a8
baoqiaoze <baoqiaoze@gmail.com>
2019-04-08 23:36:23 +0800

    190408

---
+++ src/org/eu/qiao/GetNextSolution.java
@@ -0,0 +1,23 @@
+package org.eu.qiao;
+
+/**
+ * @ClassNamg GetNextSolution
+ * @Description todo
+ * Author BOB
+ * @Date 2019/4/8 14:33
+ * @Version 1.0
+ **/
+public class GetNextSolution {
+    public TreeLinkNode GetNext(TreeLinkNode pNode){
+        TreeLinkNode res = null;
+
+        if (res == null) {
+            return null;
+        }
+
+
+
+        return  res;
+    }
+
+}
---
+++ src/org/eu/qiao/TreeLinkNode.java
@@ -0,0 +1,19 @@
+package org.eu.qiao;
+
+/**
+ * @ClassNamg TreeLinkNode
+ * @Description todo
+ * Author BOB
+ * @Date 2019/4/8 14:34
+ * @Version 1.0
+ **/
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
