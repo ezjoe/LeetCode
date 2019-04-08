dde3ff6a2bbc0a4f0ac2c83bf37bee238f01b57f
baoqiaoze <baoqiaoze@gmail.com>
2019-04-08 14:32:26 +0800

    20190408

--- src/org/eu/qiao/deleteDuplicationSolution.java
+++ src/org/eu/qiao/deleteDuplicationSolution.java
@@ -8,6 +8,20 @@ package org.eu.qiao;
  * @Version 1.0
  **/
 public class deleteDuplicationSolution {
+    public static void main(String[] args) {
+        deleteDuplicationSolution  deleteDuplicationSolution = new deleteDuplicationSolution();
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(1);
+        l1.next = l2;
+
+        ListNode res = deleteDuplicationSolution.deleteDuplication(l1);
+
+        while (res != null){
+            System.out.println(res.val);
+            res = res.next;
+        }
+
+    }
 
     public ListNode deleteDuplication(ListNode pHead){
 
@@ -15,18 +29,24 @@ public class deleteDuplicationSolution {
             return pHead;
         }
 
-        ListNode first = pHead;
-        ListNode second = pHead;
-        while (second.next != null){
-            if (second.val == second.next.val){
-                second = second.next;
-                first.next = second.next;
-            }else {
-                second = second.next;
-                first = second;
+        ListNode res = new ListNode(-1);
+        res.next = pHead;
+        ListNode p1 = res;
+        ListNode p2= pHead;
+        while (p2 != null && p2.next != null){
+            if (p2.val == p2.next.val){
+                while (p2.next != null && p2.val == p2.next.val){
+                    p2 = p2.next;
+                }
+
+                p2 = p2.next;
+                p1.next = p2;
+            }else{
+                p1 = p2;
+                p2 = p2.next;
             }
         }
-        return pHead;
+        return res.next;
 
     }
 
